1aae1e150168ac7893eb99dbab1ab10a7e9ea618
jttppn <ti_jittipon@hotmail.com>
2022-07-19 04:15:03 +0700

    init

---
+++ index.js
@@ -0,0 +1,73 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type User {
+        name:String
+        age:Int
+        position:String
+    }
+    type Query {
+        users:[User]
+    }
+    type Mutation {
+        addUser(name:String,age:Int,position:String):User,
+        deleteUser(name:String):User,
+        updateUser(name:String,age:Int,position:String,newname:String):User
+    }
+`
+
+let users = [
+    {
+        name: "Tay",
+        age: 25,
+        position: "Frontend Engineer"
+    },
+    {
+        name: "Aor",
+        age: 22,
+        position: "Backend Engineer"
+    },
+    {
+        name: "Prae",
+        age: 19,
+        position: "Fullstack Engineer"
+    },
+
+]
+
+
+const resolvers = {
+    Query: {
+        users: () => users
+    },
+    Mutation: {
+        addUser: (parent,args) => {
+            const newUser = {
+                name: args.name,
+                age: args.age,
+                position: args.position
+            }
+            users.push(newUser)
+            return newUser
+        },
+        deleteUser: (parent,args) => {
+            const user = users.find(user => user.name === args.name)
+            users = users.filter(user => user.name !== args.name)
+            return user.name
+        },
+        updateUser: (parent,args) => {
+            const user = users.find(user => user.name === args.name)
+            user.name = args.name
+            user.age = args.age
+            user.position = args.position
+            user.name = args.newname
+            return user
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
